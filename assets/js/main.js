90a4f3debe583e2d9acd99efd2a679a5d1fd8c33
hadis98 <hadisghafouri98@gmail.com>
2022-06-23 21:16:55 +0430

    handle show,hide menu[js]

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,24 @@
 /*=========== LOADER ===========*/
 /*=========== SHOW MENU ===========*/
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
 /*=========== MENU SHOW ===========*/
 /*Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
 /*=========== MENU HIDDEN ===========*/
 /*Validate if constant exists */
-
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 /*=========== REMOVE MENU MOBILE ===========*/
 /*=========== CHANGE BACKGROUND HEADER ===========*/
 /*=========== MIXITUP FILTER PRODUCTS ===========*/
