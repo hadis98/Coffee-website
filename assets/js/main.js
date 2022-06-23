887e62b2715e9bd3d11602e4c32276c4bda3716c
hadis98 <hadisghafouri98@gmail.com>
2022-06-23 21:06:21 +0430

    initial Coffe website structure

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/*=========== LOADER ===========*/
+/*=========== SHOW MENU ===========*/
+/*=========== MENU SHOW ===========*/
+/*Validate if constant exists */
+
+/*=========== MENU HIDDEN ===========*/
+/*Validate if constant exists */
+
+/*=========== REMOVE MENU MOBILE ===========*/
+/*=========== CHANGE BACKGROUND HEADER ===========*/
+/*=========== MIXITUP FILTER PRODUCTS ===========*/
+
+/*Link active products*/
+
+
+/*=========== SHOW SCROLL UP ===========*/
+/*=========== SCROLL SECTIONS ACTIVE LINK ===========*/
