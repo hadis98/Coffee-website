e95c1c240b6af0f177b07d5e500f826039bc9f9b
hadis98 <hadisghafouri98@gmail.com>
2022-06-23 21:21:07 +0430

    change background header[js],add its styles[css]

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,15 @@ const linkAction = () => {
 };
 navLinks.forEach((link) => link.addEventListener("click", linkAction));
 /*=========== CHANGE BACKGROUND HEADER ===========*/
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  if (this.scrollY >= 50) {
+    header.classList.add("scroll-header");
+  } else {
+    header.classList.remove("scroll-header");
+  }
+};
+window.addEventListener("scroll", scrollHeader);
 /*=========== MIXITUP FILTER PRODUCTS ===========*/
 
 /*Link active products*/
